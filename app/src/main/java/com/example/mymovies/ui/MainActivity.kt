e8aabbf48f053348958157f62ef857e07f5ecb4e
steeltx <realsteel.gnu@gmail.com>
2023-02-23 00:41:22 -0600

    Vista de registro

---
+++ app/src/main/java/com/example/mymovies/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mymovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/example/mymovies/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/mymovies/ui/RegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mymovies
+package com.example.mymovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mymovies.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
